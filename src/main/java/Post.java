a6748f495d7ea7aebf9523ce1f200b5e3965bbad
unknown <ruseva-s@td-glbukh.Ster-Khleb.local>
2023-01-31 08:53:28 +0500

    Inition commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,14 @@
+public class Post {
+    public class FormDate  {
+        String birthday;
+    }
+
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+
+
+}
